18a0a281bc0d89f3263de0fdfd70a84c284b8e01
Christian R. Crisologo <christiancrisologo@gmail.com>
2016-05-23 11:03:33 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log("App Started");
+$('#testContainer').text("HELLO EARTHLINGS! this part has been modified by jquery");
+console.log("$('#testContainer').text = ", $('#testContainer').val());
+// The '$' is refere as jQuery using the plugin from webpack config.js
+var config = require('./login');
---
+++ js/login.es6
@@ -0,0 +1,1 @@
+console.log('es5 started');
